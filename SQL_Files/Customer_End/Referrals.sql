85517f40bd034bd23706debb2188dda580fa2fff
stevenGarciaDev <stevgarcia2015@gmail.com>
2017-12-05 10:10:34 -0800

    Fix foreign key syntax error for Referrals table

--- SQL_Files/Customer_End/Referrals.sql
+++ SQL_Files/Customer_End/Referrals.sql
@@ -4,12 +4,12 @@ Business rules.
 	prospective customer.
 */
 
-CREATE TABLE Referrals (
+CREATE TABLE cecs323sec3og12.Referrals (
 	rFirstName VARCHAR(100) NOT NULL,
 	rLastName VARCHAR(100) NOT NULL,
 	referralDate DATE NOT NULL,
 	cID INTEGER NOT NULL,
 	rID INTEGER NOT NULL,
-	CONSTRAINT referrals_fk FOREIGN KEY (cID) REFERENCES Customers,
+	CONSTRAINT customers_fk FOREIGN KEY (cID) REFERENCES Customers (cID),
 	CONSTRAINT referral_pk PRIMARY KEY (cID, rID)
 );
