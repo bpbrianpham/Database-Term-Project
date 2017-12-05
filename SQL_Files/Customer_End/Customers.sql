8108a7c681efedb9bbc2a966ddc2e242dd0c65b7
bpham <brianthongpham@gmail.com>
2017-12-05 09:35:58 -0800

    fixed the create customers table sql

--- SQL_Files/Customer_End/Customers.sql
+++ SQL_Files/Customer_End/Customers.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `cecs323sec3og12`.`Customers` (
-  `cID` INT NOT NULL,
-  `eMail` VARCHAR(45) NULL,
-  `phoneNumber` VARCHAR(10) NULL,
-  `cDateRegistered` DATE NULL,
-  `address` VARCHAR(45) NULL,
-  PRIMARY KEY (`cID`)
+CREATE TABLE Customers (
+  cID INT NOT NULL,
+  eMail VARCHAR(45) NULL,
+  phoneNumber VARCHAR(10) NULL,
+  cDateRegistered DATE NULL,
+  address VARCHAR(45) NULL,
+  PRIMARY KEY (cID)
 );
