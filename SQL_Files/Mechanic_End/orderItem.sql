b2a39bf3fc34a8a6a2c82b3273cf292409e4d5a4
Francis Buendia <francisgbuendia@gmail.com>
2017-12-05 10:13:11 -0800

    updated order item

--- SQL_Files/Mechanic_End/orderItem.sql
+++ SQL_Files/Mechanic_End/orderItem.sql
@@ -1,5 +1,7 @@
 CREATE TABLE OrderItem (
   orderID INT NOT NULL,
   itemID INT NOT NULL,
-  PRIMARY KEY (orderID, itemID));
-
+  CONSTRAINT orderItem_pk PRIMARY KEY (orderID, itemID),
+  CONSTRAINT orderItem_fk_order FOREIGN KEY (orderID) REFERENCES MaintenanceOrder(orderID),
+  CONSTRAINT orderItem_fk_item FOREIGN KEY (itemID) REFERENCES MaintenanceItem(itemID)
+  );
