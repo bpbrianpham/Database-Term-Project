2801d857240b845c199f987206c91551c21a5f82
bpham <brianthongpham@gmail.com>
2017-12-05 16:53:26 -0800

    added more customers options

--- SQL_Files/Customer_End/CorporationAddresses.sql
+++ SQL_Files/Customer_End/CorporationAddresses.sql
@@ -8,3 +8,29 @@ CREATE TABLE CorporationAddresses (
 	FOREIGN KEY (cID)
 	REFERENCES Customers (cID)
 );
+
+INSERT INTO CorporationCustomers
+	(cID,	address,		addressType) VALUES
+	(42,	'1342 Drag Str',		''),
+    (42,	'3214 Dart Blvd',		''),
+    (42,	'6532 Cake Ln',		''),
+    (43,	'8888 ',		''),
+    (43,	'5322 ',		''),
+    (44,	'9888 ',		''),
+    (44,	'4421 ',		''),
+    (44,	'5677 ',		''),
+    (45,	'1346 ',		''),
+    (45,	'2456 ',		''),
+    (46,	'1345 ',		''),
+    (47,	'7888 ',		''),
+    (47,	'2345 ',		''),
+    (48,	'7532 ',		''),
+    (48,	'5654 ',		''),
+    (49,	'4325 ',		''),
+    (50,	'7653 ',		''),
+    (50,	'4325 ',		'');
+    
+    
+    
+    
+    
--- SQL_Files/Customer_End/CorporationCustomers.sql
+++ SQL_Files/Customer_End/CorporationCustomers.sql
@@ -8,3 +8,15 @@ CREATE TABLE CorporationCustomers (
 	FOREIGN KEY (cID)
 	REFERENCES Customers (cID)
 );
+
+INSERT INTO CorporationCustomers
+	(cID,	corpName,		corpType) VALUES
+	(42,	'Craigslist',	'Public'),
+    (43,	'Fame',			'Professional'),
+    (44,	'Wish',			'Non-Profit'),
+    (45,	'PokeStar',		'Foreign'),
+    (46,	'Fantasy',		'Professional'),
+    (47,	'Doors-R-Us',	'Public'),
+    (48,	'Windows-R-Us',	'Public'),
+    (49,	'Apple Inc.',	'Private'),
+    (50,	'Oracle',		'Private');
