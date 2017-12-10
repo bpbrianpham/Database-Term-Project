e4e88845f42c45b6e5188dba14d4cb85bf8c1c1b
stevenGarciaDev <stevgarcia2015@gmail.com>
2017-12-09 16:11:23 -0800

    Insert Referrals data

--- SQL_Files/Customer_End/Referrals.sql
+++ SQL_Files/Customer_End/Referrals.sql
@@ -13,3 +13,10 @@ CREATE TABLE cecs323sec3og12.Referrals (
 	CONSTRAINT customers_fk FOREIGN KEY (cID) REFERENCES Customers (cID),
 	CONSTRAINT referral_pk PRIMARY KEY (cID, rID)
 );
+
+INSERT INTO Referrals (rFirstName, rLastName, referralDate, cID, rID)
+	VALUES
+	('Johny', 'Bravo', '2017-12-09', 06, 1),
+	('James', 'Dean', '2017-12-15', 12, 2),
+	('Steve', 'McQueen', '2017-12-16', 15, 3)
+	('Marlon', 'Brando', '2017-12-17', 21, 4);
