2395094a3e6976fd25d6d88eb37501f4531fdfdd
bpham <brianthongpham@gmail.com>
2017-12-11 10:58:15 -0800

    adding a new column to premierCustomers

--- SQL_Files/Customer_End/PremierCustomers.sql
+++ SQL_Files/Customer_End/PremierCustomers.sql
@@ -1,6 +1,7 @@
 CREATE TABLE PremierCustomers (
   cID INT NOT NULL,
   annualFee FLOAT NULL,
+  annualSpent FLOAT NULL,
   CONSTRAINT premier_customers_pk 
 	PRIMARY KEY (cID),
   CONSTRAINT premier_customers_fk 
@@ -17,19 +18,19 @@ CREATE TABLE PremierCustomers (
 */
 
 INSERT INTO PremierCustomers
-	(cID,	annualFee) VALUES
-    (01,	48.33),
-    (02,	34.80),
-    (03,	110.40),
-    (04,	45),
-    (05,	97.06),
-    (06,	115),
-    (07,	60.75),
-    (08,	27.50),
-    (09,	96.67),
-    (10,	58),
-    (11,	6.96),
-    (12,	696),
-    (13,	62.83),
-    (14,	66.92),
-    (15,	14.5);
+	(cID,	annualFee,	annualSpent) VALUES
+    (01,	048.33,		040.13),
+    (02,	034.80,		015.44),
+    (03,	110.40,		100.53),
+    (04,	045.00,		133.64),
+    (05,	097.06,		020.11),
+    (06,	115.00,		111.88),
+    (07,	060.75,		090.32),
+    (08,	027.50,		011.00),
+    (09,	096.67,		048.00),
+    (10,	058.00,		033.00),
+    (11,	006.96,		018.00),
+    (12,	696.00,		543.11),
+    (13,	062.83,		018.33),
+    (14,	066.92,		048.22),
+    (15,	014.50,		028.99);
